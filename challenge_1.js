5e0dc3e9ab915eb3531fcfee3c91581b4141a84f
Emir Maljanovic <emir.m@imel.ba>
2018-08-13 17:49:03 +0200

    Challenge 1

---
+++ challenge_1.js
@@ -0,0 +1,13 @@
+/**
+ * Challenge
+ * 
+ * Using the JavaScript language, have the function LongestWord(sen) take the sen parameter being passed and return the largest word in the string.
+ * If there are two or more words that are the same length, return the first word from the string with that length.
+ * Ignore punctuation and assume sen will not be empty. 
+ */
+
+
+const longestWord = sen => sen.split(' ').reduce((wordOne, wordTwo) => (wordOne || '').match(/\w/g).length >= (wordTwo || '').match(/\w/g).length ? wordOne : wordTwo);
+
+console.log(longestWord('Some, string here'));
+console.log(longestWord('fun&!! longer one'));

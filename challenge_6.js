55b506fe58b444fd898390384a3b7909715b32eb
Emir Maljanovic <emir.m@imel.ba>
2018-08-16 17:54:42 +0200

    Challenge 6

---
+++ challenge_6.js
@@ -0,0 +1,12 @@
+/**
+ * Challenge
+ * Using the JavaScript language, have the function LetterCapitalize(str) take the str parameter being passed and capitalize the first letter of each word.
+ * Words will be separated by only one space. 
+ */
+
+ const letterCapitalize = (str) => str.split(' ').map(word => `${word.charAt(0).toUpperCase()}${word.substring(1, word.length)}`).join(' ');
+ console.group('Challenge 6');
+ console.log(letterCapitalize('some word here'));
+ console.log(letterCapitalize('Some other word Goin here'));
+ console.log(letterCapitalize('Another someCombo here'));
+ console.groupEnd();

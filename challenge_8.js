c8d5b9a9fa66174c06610651b197fac17a668e43
Emir Maljanovic <emir.m@imel.ba>
2018-08-20 17:56:25 +0200

    Challenge 8

---
+++ challenge_8.js
@@ -0,0 +1,13 @@
+/**
+ * Challenge
+ * Using the JavaScript language, have the function CheckNums(num1,num2) take both parameters being passed and return the string true if num2 is greater than num1, 
+ * otherwise return the string false. If the parameter values are equal to each other then return the string -1.
+ */
+
+const checkNums = (num1, num2) => num1 < num2 ? 'true' : num1 === num2 ? '-1' : 'false';
+
+console.groupCollapsed('Challenge 8');
+console.log(checkNums(3, 122));
+console.log(checkNums(67, 67));
+console.log(checkNums(15, 7));
+console.groupEnd();

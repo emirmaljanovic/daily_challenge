7817ae9e04f4a7f0ce517bb96eadedd97427af9a
Emir Maljanovic <emir.m@imel.ba>
2018-08-20 18:09:15 +0200

    Challenge 9

---
+++ challenge_9.js
@@ -0,0 +1,14 @@
+/**
+ * Challenge
+ * Using the JavaScript language, have the function TimeConvert(num) take the num parameter being passed and return the number of hours and minutes the parameter converts to 
+ * (ie. if num = 63 then the output should be 1:3). Separate the number of hours and minutes with a colon. 
+ */
+
+const timeConvert = (num) => `${Math.floor(num / 60)}:${num % 60}`;
+
+console.group('Challenge 9');
+console.log(timeConvert(63));
+console.log(timeConvert(126));
+console.log(timeConvert(520));
+console.log(timeConvert(1440));
+console.groupEnd();

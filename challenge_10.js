6dc72659f0e4ab7be4d6d4296056b2ab1e894f40
emirmaljanovic <emir.maljanovic@gmail.com>
2018-08-22 11:58:06 +0200

    Challenge 10

---
+++ challenge_10.js
@@ -0,0 +1,13 @@
+/**
+ * Challenge
+ * Using the JavaScript language, have the function AlphabetSoup(str) take the str string parameter being passed and return the string with the letters in alphabetical order 
+ * (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string. 
+ */
+
+const alphabetSoup = (str) => str.split('').sort((letter1, letter2) => letter1 < letter2 ? -1 : 1).join('');
+
+console.groupCollapsed('Challenge 10');
+console.log(alphabetSoup('hello'), alphabetSoup('hello') === 'ehllo');
+console.log(alphabetSoup('coderbyte'), alphabetSoup('coderbyte') === 'bcdeeorty');
+console.log(alphabetSoup('hooplah'), alphabetSoup('hooplah') === 'ahhloop');
+console.groupEnd();

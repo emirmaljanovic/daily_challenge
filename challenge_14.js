39020e91a9bd76c057685317ccf936c3d59f6777
emirmaljanovic <emir.maljanovic@gmail.com>
2018-08-28 13:50:48 +0200

    Challenge 14 - PentagonalNumber

---
+++ challenge_14.js
@@ -0,0 +1,17 @@
+/**
+ * Challenge
+ * 
+ * Using the JavaScript language, have the function PentagonalNumber(num) read num which will be a positive integer and determine how many dots exist in a pentagonal 
+ * shape around a center dot on the Nth iteration. For example, in the image below you can see that on the first iteration there is only a single dot, on the second iteration 
+ * there are 6 dots, on the third there are 16 dots, and on the fourth there are 31 dots. 
+ * 
+ * Your program should return the number of dots that exist in the whole pentagon on the Nth iteration. 
+ */
+
+const pentagonalNumber = num => {
+
+};
+
+console.group('Challenge 14 - PentagonalNumber');
+console.log(pentagonalNumber(2));
+console.groupEnd();

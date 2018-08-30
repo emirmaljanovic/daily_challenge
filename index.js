3b26f606b60863666b23c58a4b2c9649a97262c5
emirmaljanovic <emir.maljanovic@gmail.com>
2018-08-30 17:13:05 +0200

    Added parcel bundling

---
+++ index.js
@@ -0,0 +1,6 @@
+import longestWord from './challenges/longestWord';
+
+console.groupCollapsed('Challenge 1 - longestWord');
+console.log(longestWord('Some, string here'));
+console.log(longestWord('fun&!! longer one'));
+console.groupEnd();
